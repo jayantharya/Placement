0bfd584e69a7bf2ef273e50b2c656ef5f683bc05
jayantharya <jayantharya2782@gmail.com>
2026-06-05 21:59:15 +0530

    Day3

---
+++ Day_3/i.py
@@ -0,0 +1,36 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+arr = list(map(int, input().split()))
+sort = insertion_sort(arr)
+print(sort)
+
+# Binary
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+target = int(input())
+result = binary_search(sort, target)
+
+if result != -1:
+    print(f"array found at index {result}")
+else:
+    print("not found")
